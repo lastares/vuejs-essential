b27739b587c137ba193d3d0884d69f335fe2c642
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-07-18 14:07:30 +0800

    [feat:]添加消息组件

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import validate from './directives'
+import './directives'
+import './components'
 
 Vue.config.productionTip = false
 
