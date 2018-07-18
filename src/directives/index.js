58620d7f1a3c07bdc4d7fa83416736322aaffaaf
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-07-18 13:07:23 +0800

    [feat:]添加验证码

---
+++ src/directives/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import validate from './validator';
+
+Vue.directive('validator', validate);
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import validate from './directives'
 
 Vue.config.productionTip = false
 
